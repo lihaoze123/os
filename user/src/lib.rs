1afab826c6eef05868561b523a20260c9f59182f
chumeng <2595248810@qq.com>
2026-08-06 17:53:10 +0800

    chore: convert repository to monorepo

---
+++ user/src/lib.rs
@@ -0,0 +1,49 @@
+#![no_std]
+
+macro_rules! linker_symbol_addr {
+    ($symbol:path) => {
+        ($symbol as *const ()).addr()
+    };
+}
+
+#[macro_use]
+pub mod console;
+
+mod lang_items;
+mod syscall;
+
+unsafe extern "Rust" {
+    fn main() -> i32;
+}
+
+#[unsafe(no_mangle)]
+#[unsafe(link_section = ".text.entry")]
+pub extern "C" fn _start() -> ! {
+    clear_bss();
+    unsafe {
+        exit(main());
+    }
+    panic!("unreachable after sys_exit!");
+}
+
+fn clear_bss() {
+    unsafe extern "C" {
+        safe fn start_bss();
+        safe fn end_bss();
+    }
+    let lo = linker_symbol_addr!(start_bss);
+    let hi = linker_symbol_addr!(end_bss);
+    (lo..hi).for_each(|addr| unsafe {
+        (addr as *mut u8).write_volatile(0);
+    });
+}
+
+use crate::syscall::{sys_exit, sys_write};
+
+pub fn write(fd: usize, buf: &[u8]) -> isize {
+    sys_write(fd, buf)
+}
+
+pub fn exit(exit_code: i32) -> isize {
+    sys_exit(exit_code)
+}
